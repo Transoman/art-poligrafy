1a9ffae4b5234feccf1a79f2e79ee53477ed2f01
transoman <vasiag10@gmail.com>
2018-11-28 18:04:15 +0200

    Начал верстать страницу продукта

--- app/js/common.js
+++ app/js/common.js
@@ -70,6 +70,18 @@ jQuery(document).ready(function($) {
     }
   });
 
+  var productSlider = new Swiper ('.product__slider', {
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
   function simpleParallax(intensity, element) {
     $(window).scroll(function() {
       var scrollTop = $(window).scrollTop();
